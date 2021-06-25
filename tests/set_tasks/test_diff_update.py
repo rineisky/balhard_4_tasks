f838f5f539a3f815e3baff0ccf5f96d1573219cd
Vyacheslav Rineisky <rineisky@gmail.com>
2021-06-25 20:30:21 +0300

    fix set test

--- tests/set_tasks/test_diff_update.py
+++ tests/set_tasks/test_diff_update.py
@@ -4,4 +4,4 @@ from tasks.set_tasks.diff_update import diff_update
 def test_diff_update(new_set):
     result = diff_update(new_set, {2, 5, 7, 8, 9, 17})
     assert result is new_set
-    assert result == {1, 3, 4, 6}
+    assert result == {0, 1, 3, 4, 6}
