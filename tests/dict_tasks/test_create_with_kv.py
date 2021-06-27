f0308711a422e5fc93d01980c0fd7763884292ac
Vyacheslav Rineisky <rineisky@gmail.com>
2021-06-27 12:22:42 +0300

    fixed tests

--- tests/dict_tasks/test_create_with_kv.py
+++ tests/dict_tasks/test_create_with_kv.py
@@ -1,16 +1,17 @@
 from tasks.dict_tasks.create_with_kv import save_user
 
 
-def test_save_user(new_dict):
+def test_save_user():
     data_to_insert = {
         'login': 'user_login',
         'name': 'user_name',
         'age': 30
     }
+    login = data_to_insert["login"]
     some_dict = {}
     result = save_user(some_dict, data_to_insert)
-    assert isinstance(result["login"], dict)
-    assert result["login"] == {
+    assert isinstance(result[login], dict)
+    assert result[login] == {
         'name': 'user_name',
         'age': 30
     }
--- tests/set_tasks/test_unique_in_list.py
+++ tests/set_tasks/test_unique_in_list.py
@@ -1,5 +1,5 @@
 from tasks.set_tasks.unique_in_list import get_unique_in_list
 
 
-def test_get_unique_in_list(new_set):
-    assert test_get_unique_in_list([1, 2, 1, 3, 2]) == {1, 2, 3}
+def test_get_unique_in_list():
+    assert get_unique_in_list([1, 2, 1, 3, 2]) == {1, 2, 3}
