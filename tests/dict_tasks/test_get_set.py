ea1bb1d3d473a9389aaffa63b04eba9af2e91886
Vyacheslav Rineisky <rineisky@gmail.com>
2021-06-27 13:45:02 +0300

    fixed test get_or_set

--- tests/dict_tasks/test_get_set.py
+++ tests/dict_tasks/test_get_set.py
@@ -4,3 +4,4 @@ from tasks.dict_tasks.get_set import get_or_set
 def test_get_or_set(new_dict):
     assert get_or_set(new_dict, 2) == 2
     assert get_or_set(new_dict, 20) == 3
+    assert 20 in new_dict
