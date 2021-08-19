ddde9e6a5641bf9f583cbf98bd92b92938aa4dee
Vyacheslav Rineisky <rineisky@gmail.com>
2021-08-19 10:31:26 +0300

    fixed test get_or_set

--- tests/set_tasks/test_common_elements.py
+++ tests/set_tasks/test_common_elements.py
@@ -3,4 +3,4 @@ from tasks.set_tasks.common_elements import common_elements
 
 def test_common_elements(new_set):
     result = common_elements(new_set, {2, 5, 7, 20, -13})
-    assert result == {2, 5, 7}
+    assert result == {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 20, -13}
