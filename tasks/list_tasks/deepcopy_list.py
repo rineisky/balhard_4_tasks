e633dc41bca3d762f4ea50b7f0ca8ee7b509c3d7
marsam” <80297928585@mail.ru>
2021-09-12 00:23:41 +0300

    deep is completed

--- tasks/list_tasks/deepcopy_list.py
+++ tasks/list_tasks/deepcopy_list.py
@@ -10,6 +10,8 @@
 
 (Нужно вернуть глубокую копию данных)
 """
+
+from copy import deepcopy
 user_data = {
     "name": None,
     "surname": None
@@ -23,7 +25,7 @@ user_template = [
 
 def deepcopy_list(collection: list) -> list:
     # TODO вставить код сюда
-    collection_copy = collection
+    collection_copy = deepcopy(collection)
     return collection_copy
 
 
