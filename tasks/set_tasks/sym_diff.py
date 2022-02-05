f8193f0effa7c1ab9a169c56ca76c01c1ca28b31
Inesa Chyrok <chiriness@mail.ru>
2022-02-05 23:14:28 +0300

    sym_diff done

--- tasks/set_tasks/sym_diff.py
+++ tasks/set_tasks/sym_diff.py
@@ -6,8 +6,8 @@
 
 
 def sym_diff(set_1: set, set_2: set) -> set:
-    # TODO вставить код сюда
-    result = None
+    set_1 = set_1.symmetric_difference(set_2)
+    result = set_1
     return result
 
 
